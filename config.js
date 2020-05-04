e6e056c2e7db476db84e734cfa5fe2a1d79e9b5f
KarenBol0731 <karenbolanos0731@gmail.com>
2020-05-04 08:20:11 -0500

    adding config

---
+++ config.js
@@ -0,0 +1,10 @@
+
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/studentsdb';
+exports.TOKEN = process.env.TOKEN || 'password123';
+exports.PORT = process.env.PORT || '8080';
+
+
+
+//mongodb+srv://admin:hoshinada31@cluster0-2kwhq.mongodb.net/studentsdb?retryWrites=true&w=majority
+//process.env.DATABASE_URL ''
+///
